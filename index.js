7c3e748022d53272043ef577cb6e31ea4306e581
Łukasz Orzeł <lukaszorzell@gmail.com>
2022-07-05 20:41:06 +0200

    Saving username and hashed password in the database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const path = require('path');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
 //connect to sqlite3 db
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database/db.sqlite", sqlite3.OPEN_READWRITE);
@@ -9,6 +13,7 @@ module.exports = db;
 app.listen(3000, () => console.log('http://localhost:3000/'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
+app.use(router);
 
 
 function apiCalls() {
@@ -58,3 +63,25 @@ app.post('/foodapi', async (request, response) => {
     updateApiCallsCount();
 
 });
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/register.html'));
+});
+
+app.post('/register', (request, response) => {
+    const { username, password } = request.body;
+    bcrypt.hash(password, 10).then((hash) => {
+        db.run(`INSERT INTO users (username, u_password) VALUES ('${username}', '${hash}')`, (error, row) => {
+            if(error) {
+                console.log(error.message);
+                response.json({status: error.message});
+            } else {
+                console.log("user added to db");
+                response.json({status: 'Registered succesfully'});
+            }
+        })
+    })
+
+    
+})
+
