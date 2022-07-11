fd4ffaba12287215ff42ff099ff2d52fc620dfc4
Łukasz Orzeł <lukaszorzell@gmail.com>
2022-07-11 16:32:15 +0200

    Added simple layout and validation of username and password length.

--- public/scripts/script_register.js
+++ public/scripts/script_register.js
@@ -1,9 +1,11 @@
 const regBtn = document.getElementById('regBtn');
+let usernameField = document.getElementById('form-username');
+let passwordField = document.getElementById('form-password');
 
 
 regBtn.addEventListener('click', async function () {
-    const username = document.getElementById('form-username').value;
-    const password = document.getElementById('form-password').value;
+    let username = document.getElementById('form-username').value;
+    let password = document.getElementById('form-password').value;
 
     const data = {
         username: username,
@@ -20,8 +22,23 @@ regBtn.addEventListener('click', async function () {
 
     const response = await fetch('/register', options);
     const apiResponse = await response.json();
-    
+
     alert(apiResponse.status);
 
 
+})
+
+usernameField.addEventListener('change', function () {
+    if (this.value.length < 3) {
+        alert('Username must have at leat 63 characters');
+    }
+})
+
+passwordField.addEventListener('change', function () {
+    if (this.value.length < 8) {
+        alert('Password must be at least 8 characters long');
+        regBtn.disabled = true;
+    } else {
+        regBtn.disabled = false;
+    }
 })
